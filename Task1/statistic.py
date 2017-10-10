e6151e132f940414fa1314f1de49df562c8c5659
Nguyễn Duy Thành <ndt4081@gmail.com>
2017-10-10 08:01:25 +0700

    Mod : statistic.py

--- Task1/statistic.py
+++ Task1/statistic.py
@@ -2,19 +2,21 @@
 
 import httpagentparser
 
-#Open file
-fo = open ("gistfile1.txt", "r")
+count = 0  # count the number of user-agent
 
-#Get browser into the dict
+fo = open("gistfile1.txt", "r")  # Open file
+
+# Get browser into the dict
 browsers = dict()
 for line in fo:
+    count += 1
     os_browser = httpagentparser.simple_detect(line)
     browser = os_browser[1].split(" ")[0]
     if browser in browsers.keys():
         browsers[browser] += 1
-    else :
+    else:
         browsers[browser] = 1
 
-#Show result
-for keys, values in browsers.items():
-    print(keys, values)
+# Show result
+for key, value in browsers.items():
+    print key + ": %f" % ((value / float(count)) * 100)
