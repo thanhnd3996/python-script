2aadc019e0f9b692222621d7684edbbff4f32287
Nguyễn Duy Thành <ndt4081@gmail.com>
2017-10-10 07:59:58 +0700

    Mod : ssl_statistic.py

--- Task4/ssl_statistic.py
+++ Task4/ssl_statistic.py
@@ -2,24 +2,18 @@
 
 of = open('gistfile1.txt', 'r')
 
-protocol_list = dict()
-cipher_list = dict()
+ssl_list = dict()
+count = 0  # count the number of ssl
 
 for line in of:
-    time = line.split(' ')[0]
-    host = line.split(' ')[1]
+    count += 1
     protocol = line.split(' ')[3]
     cipher = line.split(' ')[4]
-    if protocol in protocol_list.keys():
-        protocol_list[protocol] += 1
+    ssl = protocol + "/" + cipher
+    if ssl in ssl_list.keys():
+        ssl_list[ssl] += 1
     else:
-        protocol_list[protocol] = 1
-    if cipher in cipher_list.keys():
-        cipher_list[cipher] += 1
-    else:
-        cipher_list[cipher] = 1
+        ssl_list[ssl] = 1
 
-count = 0 # count the number of protocol/cipher
-for protocol in protocol_list:
-    for cipher in cipher_list:
-
+for ssl, value in ssl_list.items():
+    print ssl + ": %f" % ((value / float(count)) * 100)
